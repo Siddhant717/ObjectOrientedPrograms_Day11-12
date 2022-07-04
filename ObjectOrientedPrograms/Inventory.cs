aad31300dd2fdc89e7ec340f70df8dc94072477c
Siddhant <santoshshrivastava39@gmail.com>
2022-07-04 13:14:50 +0530

    initial commit

---
+++ ObjectOrientedPrograms/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPrograms
+{
+    public class Inventory
+    {
+        public List<Details> TypesOfRice { get; set; }
+        public List<Details> TypesOfGrains { get; set; }
+        public List<Details> TypesOfWheats { get; set; }
+    }
+
+
+    public class Details
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+
+}
---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ObjectOrientedPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Day11&12 Assignment-2\ObjectOrientedPrograms\ObjectOrientedPrograms\Inventory.json";
+
+            FetchData fetchData = new FetchData();
+            Inventory data = fetchData.Read(path);
+
+            for (int i = 0; i < data.TypesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.TypesOfRice[i].Name);
+                Console.WriteLine(data.TypesOfRice[i].Weight);
+                Console.WriteLine(data.TypesOfRice[i].Price);
+               
+            }
+            Console.WriteLine("--------------");
+            for (int i = 0; i < data.TypesOfGrains.Count; i++)
+            {
+                Console.WriteLine(data.TypesOfGrains[i].Name);
+                Console.WriteLine(data.TypesOfGrains[i].Weight);
+                Console.WriteLine(data.TypesOfGrains[i].Price);
+                
+            }
+
+            Console.WriteLine("--------------");
+            for (int i = 0; i < data.TypesOfWheats.Count; i++)
+            {
+                Console.WriteLine(data.TypesOfWheats[i].Name);
+                Console.WriteLine(data.TypesOfWheats[i].Weight);
+                Console.WriteLine(data.TypesOfWheats[i].Price);
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+
+    
+
+        
+    
+}
