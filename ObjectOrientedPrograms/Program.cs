74896a5af288ffff588a84e8b2b183cdbf0dc35b
Siddhant <santoshshrivastava39@gmail.com>
2022-07-04 14:24:16 +0530

    Commercial Data Processing

--- ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/Program.cs
@@ -8,25 +8,7 @@ namespace ObjectOrientedPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Stock Management \n" +
-               "Enter 1 to Add new Stock\n" +
-               "Enter 2 for the Total Value of Stock");
-            int entered = int.Parse(Console.ReadLine());
-
-            StockImplementation im = new StockImplementation();
-
-            switch (entered)
-            {
-                case 1:
-                    im.AddStock();
-                    break;
-                case 2:
-                    im.ValueOfStacks();
-                    break;
-                default:
-                    Console.WriteLine("Invalid Entry");
-                    break;
-            }
+            AccountManagement.Data();
         }
     }
             
